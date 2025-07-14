3ad701b555d03b37db8029a7fb76b66ab56277d1
motasemodeh <motasem.udeh@gmail.com>
2025-07-14 13:55:02 -0400

    fix base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/e-plantShopping/', // 👈 Your GitHub repo name goes here
+  base: 'shoppingreact', // 👈 Your GitHub repo name goes here
   plugins: [react()],
 });
